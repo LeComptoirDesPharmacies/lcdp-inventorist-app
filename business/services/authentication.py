88624420d6b456fc478acc3fc62349a8548bd01c
Thomas <thomas@lecomptoirdespharmacies.fr>
2024-04-29 16:53:41 +0200

    LDS-3553 : remove useless code

--- business/services/authentication.py
+++ business/services/authentication.py
@@ -1,5 +1,3 @@
-import logging
-
 from api.consume.gen.auth.model.login_credential import LoginCredential
 from business.services.providers import get_auth_api, get_search_user_api, get_manage_api_key_api
 from business.services.security import API_KEY_NAME, persist_api_key, get_api_key
@@ -42,13 +40,6 @@ def delete_api_key():
     if isinstance(api_key_id, str):
         remove_setting(API_KEY_ID_NAME)
     elif api_key and api_key_id:
-        # if app has crashed, api_key_id stored in keyring is now a string...
-        if isinstance(api_key_id, str):
-            try:
-                api_key_id = int(api_key_id)
-            except ValueError:
-                logging.exception("api_key_id is not a number!", api_key_id)
-
         manage_api_key_api = get_manage_api_key_api()
         manage_api_key_api.delete_api_key(
             _request_auths=[manage_api_key_api.api_client.create_auth_settings("apiKeyAuth", api_key)],
